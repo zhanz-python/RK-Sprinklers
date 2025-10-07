1a2480f8dec58ed73b5bd0c82b3098cf309a3133
zhanz-python <zhanzalayahya@gmail.com>
2025-10-06 21:05:13 -0400

    Replace https://rk-sprinklers-backend.onrender.com/ with localhosts

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "https://rk-sprinklers-backend.onrender.com/",
     credentials: true,
   })
 );
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://rk-sprinklers-backend.onrender.com/",
         changeOrigin: true,
         secure: false,
       },
