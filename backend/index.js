eaef9f8c1bf0c554db1f8337bad4ef4fe2e99b4a
zhanz-python <zhanzalayahya@gmail.com>
2025-10-08 23:20:32 -0400

    index.js edit

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: ["https://rksprinklers.com", "http://localhost:5173"],
     credentials: true,
   })
 );
