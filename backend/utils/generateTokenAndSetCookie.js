2461263323b22381cbaef3bb3e8d80f59e0666ca
zhanz-python <zhanzalayahya@gmail.com>
2025-10-09 13:30:07 -0400

    Token error testing

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -8,7 +8,7 @@ export const generateTokenAndSetCookie = (res, userId) => {
     res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
